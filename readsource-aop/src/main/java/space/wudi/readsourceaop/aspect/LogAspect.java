d14ef1eceadfc524f6f259117c5ebf9cea4f48f3
discko <discko@qq.com>
2020-12-12 11:33:24 +0800

    correct advice name in aspect

--- readsource-aop/src/main/java/space/wudi/readsourceaop/aspect/LogAspect.java
+++ readsource-aop/src/main/java/space/wudi/readsourceaop/aspect/LogAspect.java
@@ -3,6 +3,7 @@ package space.wudi.readsourceaop.aspect;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import space.wudi.readsourceaop.bean.User;
 
@@ -13,11 +14,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Component
 public class LogAspect {
 
-    static AtomicInteger i = new AtomicInteger(1);
+    private static AtomicInteger i = new AtomicInteger(1);
 
     //配置切入点，所有满足条件的函数都会被加入监控
-    //@Pointcut("execution(public String space.wudi.readsourceaop.controller.MyController.getInfo(String))")
-    @Pointcut(value = "execution(* space.wudi.readsourceaop.controller..*.*(..))")
+    @Pointcut("execution(* space.wudi.readsourceaop.controller..*.*(..))")
     public void log(){}
 
     //配置Before通知
@@ -28,35 +28,35 @@ public class LogAspect {
 
     //配置Around通知
     @Around("log()")
-    public Object logAroundController(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object logAroundController(ProceedingJoinPoint joinPoint) {
         System.out.printf("%d---around before %s, args: %s\n", i.getAndIncrement(), joinPoint.getSignature(), Arrays.toString(joinPoint.getArgs()));
-        Object rtVal = null;
+        Object rtVal;
         try{
             rtVal = joinPoint.proceed();
         }catch(Throwable throwable){
             String str = throwable.toString();
             System.out.printf("%d---around after %s, catch: %s\n", i.getAndIncrement(), joinPoint.getSignature(), str);
             rtVal = "error happen because: " + str;
+
         }
         System.out.printf("%d---around after %s, returns: %s\n", i.getAndIncrement(), joinPoint.getSignature(), rtVal==null?"null":rtVal.toString());
         return rtVal;
     }
     //配置AfterReturning通知
     @AfterReturning(pointcut = "log()", returning = "rtVal")
-    public Object logAfterReturningController(JoinPoint joinPoint, Object rtVal){
+    public void logAfterReturningController(JoinPoint joinPoint, Object rtVal){
         System.out.printf("%d---after returning %s, args: %s, rtVal: %s[%s]\n", i.getAndIncrement(), joinPoint.getSignature(), Arrays.toString(joinPoint.getArgs()), rtVal.toString(), rtVal.getClass());
         //修改return的值
         if(rtVal instanceof String){
-            return ((String)rtVal)+" modified by aspect";
+            rtVal = rtVal + " modified by aspect";
         }
         if(rtVal instanceof User){
             ((User)rtVal).setEmail("modified by aspect");
         }
-        return rtVal;
     }
     //配置After通知
     @After("log()")
-    public void logController(JoinPoint joinPoint){
+    public void logAfterController(JoinPoint joinPoint){
         System.out.printf("%d---after %s, args: %s\n", i.getAndIncrement(), joinPoint.getSignature(), Arrays.toString(joinPoint.getArgs()));
     }
     //配置AfterThrowing通知
